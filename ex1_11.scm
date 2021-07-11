9cac85a49c3e2b849e7523588fa9292599d261e2
grimstir <xbio.oo@gmail.com>
2021-07-11 12:25:40 +0800

    completed 1.11

--- ex1_11.scm
+++ ex1_11.scm
@@ -1,16 +1,44 @@
-(define (f n)
+(define (f_recur n)
   (if ( < n 3)
     n
     ( + 
       ( + 
-          ( f ( - n 1 ) )
-          ( * 2 ( f (- n 2 ) ) )
+          ( f_recur ( - n 1 ) )
+          ( * 2 ( f_recur (- n 2 ) ) )
       )
-      ( * 3 ( f (- n 3 ) ) )
+      ( * 3 ( f_recur (- n 3 ) ) )
     )
   )
 )
 
+(define (iter n_1 n_2 n_3 counter max_count)
+  (
+    if ( > counter max_count ) 
+    n_1
+    (iter 
+      (+ n_1 (+ (* n_2 2 ) (* n_3 3 )))
+      n_1
+      n_2
+      (+ counter 1 )
+      max_count
+    )
+  )
+)
 
-(f 2)
-(f 6)
+(define (f_iter n)
+  (if ( < n 3)
+    n 
+    (iter 2 1 0 1 (- n 2))
+  )
+)
+
+(f_recur 2)
+(f_recur 6)
+
+(f_iter 2)
+(f_iter 6)
+
+
+
+
+
