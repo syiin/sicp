d1e4aa2c33748ed05e32d0b4426e1b33b588c91b
grimstir <xbio.oo@gmail.com>
2021-07-23 03:43:37 +0800

    update

--- exercises/ex1_45.scm
+++ exercises/ex1_45.scm
@@ -43,10 +43,26 @@
 (n_root 6 64)
 
 ;remember the order! we want to repeat average-damp ON exp_fn not repeat an average-damped exp_fn many times
-;(avg-damp(avg-damp(avg-damp exp_fn))) VS ((avg-damp exp_fn)((avg-damp exp_fn)(avg-damp exp_fn)))
+;(avg-damp(avg-damp(avg-damp exp_fn))) VS ((avg-damp (exp_fn))((avg-damp (exp_fn))(avg-damp (exp_fn))))
+; it is the difference between composing with the lambda underneath average damp and the average damp itself
 
 ;exp_fn -> avg-damp -> repeated -> fixed-point 
 ;               VS
 ;avg-damp -> repeated <- exp_fn 
 ;               |
 ;           fixed-point
+
+; Notice the below are all equivalent
+;((repeated (average-damp (exp_fn 8 3)) 2) 1)
+;((average-damp (exp_fn 8 3))((average-damp (exp_fn 8 3)) 1))
+;((average-damp (exp_fn 8 3))(/ 9 2))
+
+; Why?
+;(
+;  (average-damp (exp_fn 8 3)) ;(lambda (x) (average x ((lambda (y) ( / 8 (fast_expt y 3) )) x))))
+;  (
+;    (average-damp (exp_fn 8 3)) 1 ;((lambda (x) (average x ((lambda (y) ( / 8 (fast_expt y 3) )) x)))) 1)
+;  )                               ;(average 1 ((lambda (y) ( / 8 (fast_expt y 3) )) 1))))
+;)                                 ;(average 1  ( / 8 (fast_expt 1 3) )))))
+                                   ;9/2
+
