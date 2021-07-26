d1a4e265b9862bd342f4b45125498c711add1977
grimstir <xbio.oo@gmail.com>
2021-07-26 09:54:37 +0800

    update

--- exercises/ch2/ex2_5.scm
+++ exercises/ch2/ex2_5.scm
@@ -1,12 +1,12 @@
+; pg 154
 (define (fast_expt b n)
   (cond ((= n 0) 1)
   ((even? n) (square (fast_expt b (/ n 2))))
   (else (* b (fast_expt b (- n 1))))))
 
  (define (count-0-remainder-divisions n p d) 
-         (if (= (remainder p d) 0) 
-                 (count-0-remainder-divisions (+ n 1) (/ p d) d) 
-                 n)) 
+  (if (= (remainder p d) 0) 
+    (count-0-remainder-divisions (+ n 1) (/ p d) d)  n))
 
 (define (cons a b) (* (fast_expt 2 a) (fast_expt 3 b)))
 (define (car pair) (count-0-remainder-divisions 0 pair 2))
---
+++ exercises/ch2/ex2_6.scm
@@ -0,0 +1,18 @@
+(define zero (lambda (f) (lambda (x) x)))
+(define one (lambda (f) (lambda (x) (f x))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
+(define (add-1 n)
+  (lambda (f) (lambda (x) (f ((n f) x))))
+  )
+
+; (add-1 zero)
+; (add-1 (lambda (g) (lambda (y) y)))
+; (lambda (f) (lambda (x) (f (((lambda (g) (lambda (y) y)) f) x))))
+; (lambda (f) (lambda (x) (f ((lambda (y) y) x))))
+; (lambda (f) (lambda (x) (f x)))
+
+; (add-1 (add-1 zero))
+; (add-1 (lambda (f) (lambda (x) (f x))))
+; (lambda (f) (lambda (x) (f (((lambda (f) (lambda (x) (f x))) f) x))))
+; (lambda (f) (lambda (x) (f ((lambda (x) (f x)) x))))
+; (lambda (f) (lambda (x) (f (f x))))
