1b4d4f7a9ce6e947101c8352989838448575235a
grimstir <xbio.oo@gmail.com>
2021-07-27 10:22:00 +0800

    update

--- exercises/ch2/ex2_20.scm
+++ exercises/ch2/ex2_20.scm
@@ -1,6 +1,13 @@
 (define (same-parity . a-list)
+  (define (inner filter? a-list)
+  (cond ((null? a-list) a-list)
+        ((filter? (car a-list)) (cons (car a-list) (inner filter? (cdr a-list))) )
+        (else (inner filter? (cdr a-list)))))
+  (if (even? (car a-list))
+      (inner even? a-list)
+      (inner odd? a-list)))
 
-)
+
 
 (same-parity 1 2 3 4 5 6 7)
 (same-parity 2 3 4 5 6 7)
