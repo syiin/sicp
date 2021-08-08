a178fb23045379c2f4005cd081a021d1817f24b7
grimstir <xbio.oo@gmail.com>
2021-08-08 16:10:55 +0800

    update

--- exercises/ch2/ex2_59.scm
+++ exercises/ch2/ex2_59.scm
@@ -10,9 +10,9 @@
 
 (define (intersection-set set1 set2)
         (cond ((or (null? set1) (null? set2)) '())
-        ((element-of-set? (car set1) set2)
-        (cons (car set1) (intersection-set (cdr set1) set2)))
-        (else (intersection-set (cdr set1) set2))))
+              ((element-of-set? (car set1) set2)
+              (cons (car set1) (intersection-set (cdr set1) set2)))
+              (else (intersection-set (cdr set1) set2))))
 
 (define (union-set set1 set2)
         (cond ((or (null? set1) (null? set2)) 
---
+++ exercises/ch2/ex2_61.scm
@@ -0,0 +1,47 @@
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+    '()
+     (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+                (cons x1 (intersection-set (cdr set1) (cdr set2))))
+              ((< x1 x2)
+                 (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+                 (intersection-set set1 (cdr set2)))))))
+
+(define (adjoin-set x set)
+  (define (ordered-insert x first-half last-half)
+    (if (> x (car last-half))
+      (ordered-insert x (append first-half (list (car last-half))) (cdr last-half))
+      (append first-half (cons x last-half))))
+  (if (element-of-set? x set)
+    set
+    (ordered-insert x '() set)))
+
+(adjoin-set 3 '(1 2 4))
+
+(define (union-set set1 set2)
+  (cond ((and (null? set1) (null? set2)) '())
+         ((and (null? set1) (not (null? set2))) set2)
+         ((and (not (null? set1)) (null? set2)) set1)
+         (else (let ((x1 (car set1)) (x2 (car set2)))
+                (cond ((= x1 x2)
+                        (union-set (cdr set1) set2))
+                      ((< x1 x2)
+                        (cons x1 (union-set (cdr set1) set2)))
+                      ((< x2 x1)
+                        (cons x2 (union-set set1 (cdr set2)))))))))
+
+
+(define set-1 '(1 2 3))
+(define set-2 '(4 5 6))
+(define set-3 '(3 6 9))
+
+(union-set set-1 set-2)
+(union-set set-1 set-3)
