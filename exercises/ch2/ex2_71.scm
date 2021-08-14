4b5d1ea8c534fefc794c180de5c52f72ff6bdfe1
grimstir <xbio.oo@gmail.com>
2021-08-14 09:04:48 +0800

    update

---
+++ exercises/ch2/ex2_71.scm
@@ -0,0 +1,11 @@
+;                {a b c d e} 31
+;                /           \
+;           {a b c d} 15      e 16
+;            /     \
+;      {a b c} 7    d 8
+;        /    \
+;   {a b} 3    c 4
+;    /   \
+; a 1    b 2
+
+; minimum is 1 bit, n-1 bit os the maximum
---
+++ exercises/ch2/ex2_72.scm
@@ -0,0 +1,2 @@
+; O(log n * n) generally (ie. depth is log n and each list at every level is n long)
+; However, in 2.71, it will be O(n*n) (ie. depth is n deep and n long)
