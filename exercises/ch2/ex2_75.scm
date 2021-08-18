ecc8aa1f993495c90d158e174dfb68066c16a54a
grimstir <xbio.oo@gmail.com>
2021-08-18 11:06:52 +0800

    2.79

--- exercises/ch2/ex2_75.scm
+++ exercises/ch2/ex2_75.scm
@@ -1,12 +1,3 @@
-(define (apply-generic op . args)
-  (let ((type-tags (map type-tag args)))
-  (let ((proc (get op type-tags)))
-    (if proc
-      (apply proc (map contents args))
-      (error
-        "No method for these types: APPLY-GENERIC"
-        (list op type-tags))))))
-
 (define (make-from-mag-ang r ang)
   (define (dispatch op)
     (cond ((eq? op 'real-part) (* r (cos ang)))
---
+++ exercises/ch2/ex2_78.scm
@@ -0,0 +1,29 @@
+(define (attach-tag type-tag contents)
+  (if (number? type-tag)
+      contents
+      (cons type-tag contents)))
+
+(define (type-tag datum)
+  (cond ((number? datum) 'scheme-number)
+        ((pair? datum) (car datum))
+        (else (error "Bad tagged datum: TYPE-TAG" datum))))
+
+(define (contents datum)
+  (cond ((number? datum) datum)
+        ((pair? datum) (cdr datum))
+        (else (error "Bad tagged datum: CONTENTS" datum))))
+
+(define (install-scheme-number-package)
+  (define (tag x)
+    (attach-tag 'scheme-number x))    
+  (put 'add '(scheme-number scheme-number)
+       (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+       (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+       (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+       (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number
+       (lambda (x) (tag x)))
+  'done)
